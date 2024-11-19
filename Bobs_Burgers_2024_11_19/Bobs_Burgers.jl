cb179457697d631c5d1eb8560d95100b2b9ee75a
Nicolas Foss <ndfoss@gmail.com>
2024-11-18 23:42:15 -0600

    continued development of the julia script

--- Bobs_Burgers_2024_11_19/Bobs_Burgers.jl
+++ Bobs_Burgers_2024_11_19/Bobs_Burgers.jl
@@ -1,5 +1,6 @@
 ################################################################################
 ### Bob's Burgers Analysis!  ###################################################
+### Week 47 - 2024-11-19     ###################################################
 ################################################################################
 
 # Pkg
@@ -33,3 +34,34 @@ describe(episode_metrics)
 ### It might be interesting to see the episodes as an integer from 1:272
 ### in order to get a sort of time element without joining 
 ###_____________________________________________________________________________
+
+episode_metrics_time = @chain episode_metrics begin
+    @transform(:episode_number = 1:nrow(episode_metrics)) # Add a new column with sequential numbers
+end
+
+###_____________________________________________________________________________
+### Let's examine dialogue density over time from Bob's Burgers
+###_____________________________________________________________________________
+
+# plot theme
+theme(:gruvbox_light)  # Apply gruvbox theme
+
+# pivot the dataframe longer using the exclamation and question mark fields
+
+episode_metrics_pivot = @chain begin episode_metrics_time
+    stack([:question_ratio, :exclamation_ratio])
+end
+
+# use a line plot to show the prevalence of excitement or throught provoking
+# questions in Bob's Burgers over time
+
+questions_or_exclamations_plot = @df episode_metrics_pivot plot(
+    :episode_number,
+    :value,
+    group = :variable,
+    grid = false,
+    legend_position = :outsidetopright,
+    legend_title = "Punctuation",
+    title="Question Marks vs. Exclamation Points: The Battle of Bob's Burgers Dialogue!",
+
+)
